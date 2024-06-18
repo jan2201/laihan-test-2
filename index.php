69c7d6e8f23a7ef89d441834c95c81270ea0ce96
jan2201 <aojan15.aojan@gmail.com>
2024-06-18 22:16:54 +0700

    menambahkan list

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>selamat pagi</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, officia fugit! Cum sapiente, dolor deserunt velit dolorum accusantium fugit repellat dolorem veniam? Voluptas dicta hic, dolores aliquid aut et! Amet.</p>
+    <ul>
+        <li>selamat</li>
+        <li>pagi</li>
+    </ul>
+</body>
+</html>
